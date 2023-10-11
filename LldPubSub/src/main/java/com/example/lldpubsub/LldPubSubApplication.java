49d6247236c114c15d97924e4a2b8dbb22d6d179
nbagade <niranjan.bagade@nice.com>
2023-10-11 09:54:53 +0530

    Create blank project for pub-sub

---
+++ LldPubSub/src/main/java/com/example/lldpubsub/LldPubSubApplication.java
@@ -0,0 +1,13 @@
+package com.example.lldpubsub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LldPubSubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LldPubSubApplication.class, args);
+    }
+
+}
---
+++ LldPubSub/src/test/java/com/example/lldpubsub/LldPubSubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lldpubsub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LldPubSubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
